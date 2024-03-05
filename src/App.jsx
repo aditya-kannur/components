c22d19d00295f4493249549f8b6d87b9288c0f45
AdityaKannur <adityakannur28@gmail.com>
2024-03-05 22:20:02 +0530

    used components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import GallaryFooter from './components/GallaryFooter';
+import GallaryHeader from './components/GalllaryHeader';
+import GallaryBody from './components/GallaryBody';
+
+
+
+function App() {
+  return (
+    <div>
+      <GallaryHeader/>
+      <GallaryBody/>
+      <GallaryFooter/> 
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/GallaryFooter.jsx
@@ -0,0 +1,7 @@
+export default function GallaryFooter(){
+  return(
+    <footer className='footer-content'>
+      <h3>Author: Kalvium</h3>
+    </footer>
+  )
+}
